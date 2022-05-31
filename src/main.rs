71bab0e66c19023c54a2504124c64574a6a216cc
Noah Wahl <noah.ares@yahoo.de>
2022-05-31 14:59:49 +0200

    Commit progress up to post-05

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(rust_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use rust_os::println;
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default
+
+    // safe write through dedicated interface
+    println!("Hello World{}", "!");
+
+    // panic
+    // panic!("Some panic message");
+
+    // tests
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
+
+// This function is called on panic.
+#[cfg(not(test))] // conditional compilation
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rust_os::test_panic_handler(info)
+}
